467494eb02e3ec4f93d08c46045ca4dfd21eb30b
Kristoffer Strube <kristoffer.strube@gmail.com>
2020-06-22 22:58:28 +0200

    Revert "Working on implementing ImageData methids. Get works, but Put needs more work"
    This reverts commit 7588bb1b6be621bdf369191299e7554d65015ad0.

--- test/Blazor.Extensions.Canvas.Test.ClientSide/Pages/IndexComponent.cs
+++ test/Blazor.Extensions.Canvas.Test.ClientSide/Pages/IndexComponent.cs
@@ -1,8 +1,5 @@
 using Blazor.Extensions.Canvas.Canvas2D;
 using Microsoft.AspNetCore.Components;
-using System;
-using System.Linq;
-using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Blazor.Extensions.Canvas.Test.ClientSide.Pages
@@ -18,17 +15,10 @@ namespace Blazor.Extensions.Canvas.Test.ClientSide.Pages
             this._context = await this._canvasReference.CreateCanvas2DAsync();
             await this._context.SetFillStyleAsync("green");
 
-            await this._context.FillRectAsync(0, 0, 100, 100);
+            await this._context.FillRectAsync(10, 100, 100, 100);
 
             await this._context.SetFontAsync("48px serif");
             await this._context.StrokeTextAsync("Hello Blazor!!!", 10, 100);
-
-            var ImageData = await this._context.GetImageDataAsync(0, 0, 100, 100);
-
-            ImageData.Data = new byte[2];
-
-            await this._context.PutImageDataAsync(ImageData, 0, 0);
-            
         }
     }
 }
