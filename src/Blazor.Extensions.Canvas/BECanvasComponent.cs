df13caac2e3838964c8b9e369064837544fe9c6d
BuildTools <unconfigured@null.spigotmc.org>
2022-07-21 10:41:14 -0400

    Update nuget packaging information. Fix package-lock. Passthrough click, contextMenu, and mouseWheel events. Provide method to specify to preventDefault and stopPropagation on contextMenu event.

--- src/Blazor.Extensions.Canvas/BECanvasComponent.cs
+++ src/Blazor.Extensions.Canvas/BECanvasComponent.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.JSInterop;
 using System;
+using System.Threading.Tasks;
 
 namespace Blazor.Extensions
 {
@@ -13,7 +15,19 @@ namespace Blazor.Extensions
         public long Width { get; set; }
 
         [Parameter]
+        public bool ContextMenuPreventDefault { get; set; } = false;
+
+        [Parameter]
+        public bool ContextMenuStopPropagation { get; set; } = false;
+
+        [Parameter]
         public EventCallback<MouseEventArgs> Click { get; set; }
+
+        [Parameter]
+        public EventCallback<MouseEventArgs> ContextMenu { get; set; }
+
+        [Parameter]
+        public EventCallback<WheelEventArgs> MouseWheel { get; set; }
 
         protected readonly string Id = Guid.NewGuid().ToString();
         protected ElementReference _canvasRef;
@@ -23,9 +37,14 @@ namespace Blazor.Extensions
         [Inject]
         internal IJSRuntime JSRuntime { get; set; }
 
-        protected async Task Clicked(MouseEventArgs args)
+        protected async Task OnClicked(MouseEventArgs args)
         {
-          await Click.InvokeAsync();
+          await this.Click.InvokeAsync(args);
+        }
+
+        protected async Task OnContextMenu(MouseEventArgs args)
+        {
+            await this.ContextMenu.InvokeAsync(args);
         }
     }
 }
