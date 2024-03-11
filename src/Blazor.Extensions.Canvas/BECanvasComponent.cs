a62f08616b09060abfb68c3eeb17efbb9e27309b
shawn <shawn@frozen.ca>
2024-03-11 17:39:09 -0500

    Made the ID a parameter.

--- src/Blazor.Extensions.Canvas/BECanvasComponent.cs
+++ src/Blazor.Extensions.Canvas/BECanvasComponent.cs
@@ -7,12 +7,14 @@ namespace Blazor.Extensions
     public class BECanvasComponent : ComponentBase
     {
         [Parameter]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Parameter]
         public long Height { get; set; }
 
         [Parameter]
         public long Width { get; set; }
 
-        protected readonly string Id = Guid.NewGuid().ToString();
         protected ElementReference _canvasRef;
 
         public ElementReference CanvasReference => this._canvasRef;
